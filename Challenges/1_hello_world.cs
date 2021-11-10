c8b46eb396fc33d4b9de75eb3971d92c464c25dc
MecIntyre <abuesswalker@gmail.com>
2021-11-10 13:57:11 +0100

    hello world challenge

---
+++ Challenges/1_hello_world.cs
@@ -0,0 +1,17 @@
+// Output: This is the first line.
+//         This is the second line.
+
+// Use the two different techniques to print the two lines of the output. 
+// In other words, you learned how to display a message in just one line of code, 
+// and you learned how to display a message using multiple lines of code. Use both techniques for this challenge. 
+// It doesn't matter which technique you apply to which line, 
+// and it doesn't matter how many ways you split one of the messages into multiple lines of code. 
+// That's your choice.
+
+
+// One possible solution:
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");

f76931ff549fb6a2d1c9fec8dce532b9db118ae5
MecIntyre <abuesswalker@gmail.com>
2021-11-10 13:57:34 +0100

    variable and value exercise

---
+++ Exercies/2_iteral_variable_values.cs
@@ -0,0 +1,20 @@
+// A literal value is a hard-coded value that never changes. 
+// Previously, we displayed a literal string to the Output pane. 
+// In other words, we literally wanted that string of alphanumeric characters H, e, l, l, o, 
+// and so on, displayed in the Output window.
+
+// string, for words, phrases, or any alphanumeric data for presentation, not calculation
+Console.WriteLine("123");
+
+// char for a single alphanumeric character
+Console.WriteLine('B');
+
+// int (integer), for a whole number
+Console.WriteLine(123);
+
+// decimal, for a number with a decimal
+Console.WriteLine(12.3m);
+
+// bool, for a true/false value
+Console.WriteLine(true);
+Console.WriteLine(false);

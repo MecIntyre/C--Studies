bf6ebc03e79598b4f93b76fd43e50f0d44e3256a
MecIntyre <abuesswalker@gmail.com>
2021-11-15 09:45:18 +0100

    complicated permissions challenge

---
+++ Challenges/10_complicated_permission.cs
@@ -0,0 +1,47 @@
+/* Implement business rules that restrict access to users based on their permissions and their level. 
+   You'll display a different message to the user depending on their permissions and level.
+
+   1. If the user is an Admin with a level greater than 55, output the message:
+      Output: Welcome, Super Admin user.
+   2. If the user is an Admin with a level less than or equal to 55, output the message:
+      Output: Welcome, Admin user.
+   3. If the user is a Manager with a level 20 or greater, output the message:
+      Output: Contact an Admin for access.
+   4. If the user is a Manager with a level less than 20, output the message:
+      Output: You do not have sufficient privileges.
+   5. If the user is not an Admin or a Manager, output the message:
+      Output: You do not have sufficient privileges. 
+   6. When you run the code, including the sample data from Step 2, you should see the following output:
+      Output: Welcome, Admin user. */
+
+
+//Solution: 
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
